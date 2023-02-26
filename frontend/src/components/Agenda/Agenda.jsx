e20854f4bce86f8b8755629116145fa914945253
salaboy <Salaboy@gmail.com>
2023-02-26 10:15:36 +0000

    adding proxy

--- frontend/src/components/Agenda/Agenda.jsx
+++ frontend/src/components/Agenda/Agenda.jsx
@@ -8,6 +8,22 @@ export default function Agenda(props) {
     const [agendaItems, setAgendaItems] = useState('') 
     const {day, highlights} = props;
 
+
+    const mockAgendaItemsMonday = [{
+        "title": "Cached Entry",
+        "author": "Cached Author",
+        "time": "1pm",
+        "day": "Monday",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id officia doloribus, molestiae, mollitia quia maiores velit consequuntur dolorem labore beatae, porro aliquam quis! Quasi commodi aperiam, assumenda rem molestiae porro."
+    }]
+    const mockAgendaItemsTuesday = [{
+        "title": "Cached Entry",
+        "author": "Cached Author",
+        "time": "1pm",
+        "day": "Tuesday",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id officia doloribus, molestiae, mollitia quia maiores velit consequuntur dolorem labore beatae, porro aliquam quis! Quasi commodi aperiam, assumenda rem molestiae porro."
+    }]
+
     useEffect(() => {                           // side effect hook
         var dayString = ''
         if (day) {
@@ -18,7 +34,7 @@ export default function Agenda(props) {
         console.log("Querying with: " + "/agenda/" + dayString)
         axios({
             "method": "GET",
-            "url": "/agenda/" + dayString, // This is going throw the proxy in package.json
+            "url": "/api/agenda/" + dayString, // This is going throw the proxy in package.json
             "headers": {}, "params": {}
         })
             .then((response) => {
@@ -39,7 +55,7 @@ export default function Agenda(props) {
         <div className="Agenda">   
             Agenda Component {appStatus.toString()}
             Agenda Items: 
-            {agendaItems}
+            {agendaItems.toString}
         </div>
     )
 }
--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -2,10 +2,13 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    '/',
+    '/api/',
     createProxyMiddleware({
       target: 'http://fmtok8s-frontend.default.35.204.218.48.sslip.io',
       changeOrigin: true,
+      pathRewrite: {
+        '^/api/': '/', // remove base path
+      },
     })
   );
 };
